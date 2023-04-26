ac3426556810c81d2cf571b72905ab0263bb569b
Paul <moicoxoxo@gmail.com>
2023-04-26 11:59:36 -0400

    change url  to https

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -5,14 +5,12 @@ const API_KEY = '31d642da'
 export const getMovies = (search) => {
   if (search === '') return
 
-  return fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+  return fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
     .then((response) => response.json())
     .then((response) => {
       return getMoviesAdapter(response.Search)
     })
-    .catch((e) => {
-      console.log('TCL: getMovies -> e', e)
-
+    .catch(() => {
       throw new Error('Error searching movies')
     })
 }
